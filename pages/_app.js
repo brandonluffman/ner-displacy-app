0d87f20f9c4fdb7c1c6983c23864e588763b59a4
Brandon Luffman <brandonluff10@gmail.com>
2023-05-28 07:34:25 -0400

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import '../styles/index.css'
+
+{/* {responseData && <div>{JSON.stringify(responseData[1])}</div>}
+{responseData && <div>Response: {JSON.stringify(responseData[0])}</div>} */}
+
+  // responseData && responseData[0].map((item, i) => (
+  //   console.log(item.start, item.end)
+  // ))
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
